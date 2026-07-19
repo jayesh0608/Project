7bef633981f844676bf44d64f406e7da88260bdd
Your Name <you@example.com>
2026-07-19 18:13:38 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './components/Hello';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <Hello />
+        </div>
+    );
+}
+
+export default App;
